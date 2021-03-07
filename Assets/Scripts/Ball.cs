5f2f18850cb6459460c14bbe0a0b0ae6619c0dfc
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2021-03-07 11:34:13 +0300

    init

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D rgdBody;
+
+    private void Start()
+    {
+        rgdBody = GetComponent<Rigidbody2D>();
+        RandomPush();
+    }
+
+    public void RandomPush()
+    {
+        var random = Random.Range(0f, 1f);
+        int x = random > 0.5f ? 1 : -1;
+        int y = random < 0.5f ? 1 : -1;
+        rgdBody.AddForce(new Vector2(x, y) * speed);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        transform.position = Vector2.zero;
+        rgdBody.velocity = Vector2.zero;
+        RandomPush();
+    }
+}
---
+++ Assets/Scripts/Racket.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            PushLeft();
+        else if (Input.GetKey(KeyCode.D))
+            PushRight();
+    }
+
+    public void PushRight()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+
+    public void PushLeft()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        transform.position = new Vector2(-transform.position.x, transform.position.y);
+    }
+}
