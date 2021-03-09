fbc8a5318547238100592c78e146d3201ab0dffc
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2021-03-09 23:05:23 +0300

    input decomposition

--- Assets/Scripts/Racket.cs
+++ Assets/Scripts/Racket.cs
@@ -3,13 +3,19 @@
 public class Racket : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private InputManager manager;
 
-    private void Update()
+    private void Start()
     {
-        if (Input.GetKey(KeyCode.A))
-            Push(Vector2.left);
-        else if (Input.GetKey(KeyCode.D))
+        manager.Moving.AddListener(Push);
+    }
+
+    public void Push(float direction)
+    {
+        if (direction > 0)
             Push(Vector2.right);
+        else
+            Push(Vector2.left);
     }
 
     private void Push(Vector3 direction)
