1b3e7df0d1a12275b8416c904f15ca043e75bb5b
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2021-03-09 23:15:46 +0300

    input fix

---
+++ Assets/Scripts/Managers/RacketManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class RacketManager : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Racket up;
+    [SerializeField] private Racket down;
+    private float currentAxis;
+
+    private void Update()
+    {
+        currentAxis = Input.GetAxis("Horizontal");
+        if (currentAxis != 0)
+        {
+            var vector = new Vector3(currentAxis, 0, 0);
+            up.Push(vector, speed);
+            down.Push(vector, speed);
+        }
+    }
+}
--- Assets/Scripts/Racket.cs
+++ Assets/Scripts/Racket.cs
@@ -2,23 +2,7 @@
 
 public class Racket : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    [SerializeField] private InputManager manager;
-
-    private void Start()
-    {
-        manager.Moving.AddListener(Push);
-    }
-
-    public void Push(float direction)
-    {
-        if (direction > 0)
-            Push(Vector2.right);
-        else
-            Push(Vector2.left);
-    }
-
-    private void Push(Vector3 direction)
+    public void Push(Vector3 direction, float speed)
     {
         transform.position += direction * speed * Time.deltaTime;
     }
