4e926f55488f2124cef60d8e86f3a0937ce11339
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2021-03-09 23:53:58 +0300

    adjuster fix

--- Assets/Scripts/FieldAdjuster.cs
+++ Assets/Scripts/FieldAdjuster.cs
@@ -2,38 +2,50 @@ using UnityEngine;
 
 public class FieldAdjuster : MonoBehaviour
 {
-    [SerializeField] BoxCollider2D playField;
-    [SerializeField] BoxCollider2D wallLeft;
-    [SerializeField] BoxCollider2D wallRight;
-    [SerializeField] BoxCollider2D zoneUp;
-    [SerializeField] BoxCollider2D zoneDown;
+    [SerializeField] private BoxCollider2D playField;
+    [SerializeField] private BoxCollider2D wallLeft;
+    [SerializeField] private BoxCollider2D wallRight;
+    [SerializeField] private BoxCollider2D zoneUp;
+    [SerializeField] private BoxCollider2D zoneDown;
+    private Vector2 lastCheckedCameraSize;
 
     private void Start()
     {
-        GetCameraSize(out float height, out float width);
-        AdjustBorders(height, width);
+        lastCheckedCameraSize = GetCameraSize();
+        AdjustBorders(lastCheckedCameraSize);
     }
 
-    private void GetCameraSize(out float height, out float width)
+    private void FixedUpdate()
+    {
+        if (lastCheckedCameraSize != GetCameraSize())
+        {
+            lastCheckedCameraSize = GetCameraSize();
+            AdjustBorders(lastCheckedCameraSize);
+        }
+    }
+
+    private Vector2 GetCameraSize()
     {
         Camera camera = Camera.main;
 
-        height = 2f * camera.orthographicSize;
-        width = height * camera.aspect;
+        var height = 2f * camera.orthographicSize;
+        var width = height * camera.aspect;
+
+        return new Vector2(width, height);
     }
 
-    private void AdjustBorders(float height, float width)
+    private void AdjustBorders(Vector2 cameraSize)
     {
-        playField.size = new Vector2(width, height);
+        playField.size = cameraSize;
 
-        Vector2 wallSize = new Vector2(1, height);
-        Vector2 wallOffset = new Vector2(width * 0.5f, 0);
+        Vector2 wallSize = new Vector2(1, cameraSize.y);
+        Vector2 wallOffset = new Vector2(cameraSize.x * 0.5f, 0);
 
         Adjust(wallRight, wallSize, wallOffset);
         Adjust(wallLeft, wallSize, -wallOffset);
 
-        Vector2 zoneSize = new Vector2(width, 0.5f);
-        Vector2 zoneOffset = new Vector2(0, height * 0.5f);
+        Vector2 zoneSize = new Vector2(cameraSize.x, 0.5f);
+        Vector2 zoneOffset = new Vector2(0, cameraSize.y * 0.5f);
 
         Adjust(zoneUp, zoneSize, zoneOffset);
         Adjust(zoneDown, zoneSize, -zoneOffset);
