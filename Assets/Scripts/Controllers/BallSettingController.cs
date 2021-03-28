4a0ce41e00d623f778a69d7792b89fe58134d4b3
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2021-03-28 16:00:55 +0300

    clean up

--- Assets/Scripts/Controllers/BallSettingController.cs
+++ Assets/Scripts/Controllers/BallSettingController.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class BallSettingController : MonoBehaviour
 {
+    SettingsMenuView view;
     [SerializeField] BallSettings ballSettings;
     [SerializeField] MenuManager menuManager;
-    SettingsMenuView view;
 
     private void Awake()
     {
